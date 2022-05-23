9b064a4bc1f39a87c336ff5cfd7746a829cbd101
kodenett <karnet1225@my.sullivan.edu>
2022-05-23 18:55:40 -0400

    Created help actions, exit actions, and labeled every form and button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSC240_09_02_TicTacToe_kpa
 {
-    partial class Form1
+    partial class UxTicTacToeForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,207 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.UxmenuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.UxfileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UxstartNewGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UxexitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UxfileToolStripMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.UxhelpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.UxaboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Uxbutton1 = new System.Windows.Forms.Button();
+            this.Uxbutton2 = new System.Windows.Forms.Button();
+            this.Uxbutton3 = new System.Windows.Forms.Button();
+            this.Uxbutton4 = new System.Windows.Forms.Button();
+            this.Uxbutton5 = new System.Windows.Forms.Button();
+            this.Uxbutton6 = new System.Windows.Forms.Button();
+            this.Uxbutton7 = new System.Windows.Forms.Button();
+            this.Uxbutton8 = new System.Windows.Forms.Button();
+            this.Uxbutton9 = new System.Windows.Forms.Button();
+            this.UxmenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // UxmenuStrip1
+            // 
+            this.UxmenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UxfileToolStripMenuItem,
+            this.UxhelpToolStripMenuItem});
+            this.UxmenuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.UxmenuStrip1.Name = "UxmenuStrip1";
+            this.UxmenuStrip1.Size = new System.Drawing.Size(451, 24);
+            this.UxmenuStrip1.TabIndex = 0;
+            this.UxmenuStrip1.Text = "menuStrip1";
+            this.UxmenuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // UxfileToolStripMenuItem
+            // 
+            this.UxfileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UxstartNewGameToolStripMenuItem,
+            this.UxexitToolStripMenuItem,
+            this.UxfileToolStripMenu});
+            this.UxfileToolStripMenuItem.Name = "UxfileToolStripMenuItem";
+            this.UxfileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.UxfileToolStripMenuItem.Text = "File";
+            // 
+            // UxstartNewGameToolStripMenuItem
+            // 
+            this.UxstartNewGameToolStripMenuItem.Name = "UxstartNewGameToolStripMenuItem";
+            this.UxstartNewGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.UxstartNewGameToolStripMenuItem.Text = "Start New Game";
+            // 
+            // UxexitToolStripMenuItem
+            // 
+            this.UxexitToolStripMenuItem.Name = "UxexitToolStripMenuItem";
+            this.UxexitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.UxexitToolStripMenuItem.Text = "Exit";
+            this.UxexitToolStripMenuItem.Click += new System.EventHandler(this.UxexitToolStripMenuItem_Click);
+            // 
+            // UxfileToolStripMenu
+            // 
+            this.UxfileToolStripMenu.Name = "UxfileToolStripMenu";
+            this.UxfileToolStripMenu.Size = new System.Drawing.Size(180, 22);
+            this.UxfileToolStripMenu.Text = "File";
+            // 
+            // UxhelpToolStripMenuItem
+            // 
+            this.UxhelpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.UxaboutToolStripMenuItem});
+            this.UxhelpToolStripMenuItem.Name = "UxhelpToolStripMenuItem";
+            this.UxhelpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.UxhelpToolStripMenuItem.Text = "Help";
+            // 
+            // UxaboutToolStripMenuItem
+            // 
+            this.UxaboutToolStripMenuItem.Name = "UxaboutToolStripMenuItem";
+            this.UxaboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.UxaboutToolStripMenuItem.Text = "About";
+            this.UxaboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // Uxbutton1
+            // 
+            this.Uxbutton1.Location = new System.Drawing.Point(75, 55);
+            this.Uxbutton1.Name = "Uxbutton1";
+            this.Uxbutton1.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton1.TabIndex = 1;
+            this.Uxbutton1.UseVisualStyleBackColor = true;
+            this.Uxbutton1.Click += new System.EventHandler(this.Uxbutton1_Click);
+            // 
+            // Uxbutton2
+            // 
+            this.Uxbutton2.Location = new System.Drawing.Point(176, 55);
+            this.Uxbutton2.Name = "Uxbutton2";
+            this.Uxbutton2.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton2.TabIndex = 2;
+            this.Uxbutton2.UseVisualStyleBackColor = true;
+            this.Uxbutton2.Click += new System.EventHandler(this.Uxbutton2_Click);
+            // 
+            // Uxbutton3
+            // 
+            this.Uxbutton3.Location = new System.Drawing.Point(277, 55);
+            this.Uxbutton3.Name = "Uxbutton3";
+            this.Uxbutton3.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton3.TabIndex = 3;
+            this.Uxbutton3.UseVisualStyleBackColor = true;
+            this.Uxbutton3.Click += new System.EventHandler(this.Uxbutton3_Click);
+            // 
+            // Uxbutton4
+            // 
+            this.Uxbutton4.Location = new System.Drawing.Point(75, 152);
+            this.Uxbutton4.Name = "Uxbutton4";
+            this.Uxbutton4.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton4.TabIndex = 4;
+            this.Uxbutton4.UseVisualStyleBackColor = true;
+            this.Uxbutton4.Click += new System.EventHandler(this.Uxbutton4_Click);
+            // 
+            // Uxbutton5
+            // 
+            this.Uxbutton5.Location = new System.Drawing.Point(176, 152);
+            this.Uxbutton5.Name = "Uxbutton5";
+            this.Uxbutton5.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton5.TabIndex = 5;
+            this.Uxbutton5.UseVisualStyleBackColor = true;
+            this.Uxbutton5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Uxbutton6
+            // 
+            this.Uxbutton6.Location = new System.Drawing.Point(277, 152);
+            this.Uxbutton6.Name = "Uxbutton6";
+            this.Uxbutton6.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton6.TabIndex = 6;
+            this.Uxbutton6.UseVisualStyleBackColor = true;
+            this.Uxbutton6.Click += new System.EventHandler(this.Uxbutton6_Click);
+            // 
+            // Uxbutton7
+            // 
+            this.Uxbutton7.Location = new System.Drawing.Point(75, 249);
+            this.Uxbutton7.Name = "Uxbutton7";
+            this.Uxbutton7.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton7.TabIndex = 7;
+            this.Uxbutton7.UseVisualStyleBackColor = true;
+            this.Uxbutton7.Click += new System.EventHandler(this.Uxbutton7_Click);
+            // 
+            // Uxbutton8
+            // 
+            this.Uxbutton8.Location = new System.Drawing.Point(176, 249);
+            this.Uxbutton8.Name = "Uxbutton8";
+            this.Uxbutton8.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton8.TabIndex = 8;
+            this.Uxbutton8.UseVisualStyleBackColor = true;
+            this.Uxbutton8.Click += new System.EventHandler(this.Uxbutton8_Click);
+            // 
+            // Uxbutton9
+            // 
+            this.Uxbutton9.Location = new System.Drawing.Point(277, 249);
+            this.Uxbutton9.Name = "Uxbutton9";
+            this.Uxbutton9.Size = new System.Drawing.Size(95, 91);
+            this.Uxbutton9.TabIndex = 9;
+            this.Uxbutton9.UseVisualStyleBackColor = true;
+            this.Uxbutton9.Click += new System.EventHandler(this.Uxbutton9_Click);
+            // 
+            // UxTicTacToeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(451, 381);
+            this.Controls.Add(this.Uxbutton9);
+            this.Controls.Add(this.Uxbutton8);
+            this.Controls.Add(this.Uxbutton7);
+            this.Controls.Add(this.Uxbutton6);
+            this.Controls.Add(this.Uxbutton5);
+            this.Controls.Add(this.Uxbutton4);
+            this.Controls.Add(this.Uxbutton3);
+            this.Controls.Add(this.Uxbutton2);
+            this.Controls.Add(this.Uxbutton1);
+            this.Controls.Add(this.UxmenuStrip1);
+            this.MainMenuStrip = this.UxmenuStrip1;
+            this.Name = "UxTicTacToeForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.UxTicTacToeForm_Load);
+            this.UxmenuStrip1.ResumeLayout(false);
+            this.UxmenuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip UxmenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem UxfileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UxstartNewGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UxexitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UxhelpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem UxaboutToolStripMenuItem;
+        private System.Windows.Forms.Button Uxbutton1;
+        private System.Windows.Forms.Button Uxbutton2;
+        private System.Windows.Forms.Button Uxbutton3;
+        private System.Windows.Forms.Button Uxbutton4;
+        private System.Windows.Forms.Button Uxbutton5;
+        private System.Windows.Forms.Button Uxbutton6;
+        private System.Windows.Forms.Button Uxbutton7;
+        private System.Windows.Forms.Button Uxbutton8;
+        private System.Windows.Forms.Button Uxbutton9;
+        private System.Windows.Forms.ToolStripMenuItem UxfileToolStripMenu;
     }
 }
