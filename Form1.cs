c46d82ce5679c85c0d2fe5ced90c43292b7cd871
kodenett <karnet1225@my.sullivan.edu>
2022-06-06 23:09:50 -0400

    Gave the event to start a new game possible without having to exit the application, and finished project!

--- Form1.cs
+++ Form1.cs
@@ -233,7 +233,20 @@ namespace CSC240_09_02_TicTacToe_kpa
 
         private void UxstartNewGameToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            turn = true;
+            turn_count = 0;
 
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = true;
+                    b.Text = "";
+                }
+
+            }
+            catch { }
         }
     }
     }
